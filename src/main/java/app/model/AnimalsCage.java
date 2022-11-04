f2d2b9d38abb0557af814bf32202ea7e59e993b3
SerDes010 <sdesyatoff@gmail.com>
2022-11-04 19:38:33 +0300

    первая версия задачи 2.1.4

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,27 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    public AnimalsCage() {
+        this(null);
+    }
+
+    public AnimalsCage(Animal animal) {
+        this.animal = animal;
+    }
+
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +29,17 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    @Bean("dog")
+    public Dog getDog() {
+        return new Dog();
+    }
+
+    @Bean()
+    public Timer getTimer() {
+        Timer timer1;
+        timer1 = timer;
+        return timer1;
     }
 }
